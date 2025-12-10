8f98e54602cbd4bea6e85e3c3d210fb1dd95274b
ManuelGonzalez2 <9104612@alumnos.ufv.es>
2025-12-10 19:17:11 +0100

    Update citas.py

--- src/citas.py
+++ src/citas.py
@@ -1,14 +1,17 @@
-from datetime import date 
-
+from datetime import date
 from .mascotas import Mascota
+from .clientes import Cliente 
 
 class Cita:
+    """
+    Representa una cita médica programada en la clínica.
+    """
     def __init__(self, fecha: date, hora: str, motivo: str, veterinario: str, mascota: Mascota):
         self.fecha = fecha
         self.hora = hora
         self.motivo = motivo
         self.veterinario = veterinario
         self.mascota = mascota
-
-    def __str__(self):
-        return f"Usted tiene cita el {self.fecha} a las {self.hora} para {self.mascota.nombre}"
+        
+        # Genera un ID único basado en fecha, hora y el nombre de la mascota
+        self.id_cita = f"{fecha}_{hora}_{mascota.nombre}"

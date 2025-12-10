dca2692805df79c1e9fe45f8752dcc07c71d5c86
ManuelGonzalez2 <9104612@alumnos.ufv.es>
2025-12-10 19:22:12 +0100

    Update Home.py

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -2,7 +2,8 @@
 import streamlit as st
 import sys
 import os
-
+# La siguiente linea de codigo es un ajuste de ruta para que pueda encontrar la carpeta src
+# Es la solución de emergencia para que los ficheros de pages reconozcan al motor.
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from src.veterinaria import Veterinaria
@@ -11,6 +12,9 @@ st.set_page_config(page_title="Curae Veterinaria")
 
 st.title("Curae Veterinaria - Inicio de Sesión")
 
+# Aqui utilizamos el patron de singleton o memoria persistente, streamlit lo que hace es que cada vez que 
+# un usuario hace clic, este se reinicia. Utilizamos la funcion st.session_state para que le diga a streamlit
+# todo lo que pongamos se quede en su memoria.
 if "mi_clinica" not in st.session_state:
     st.session_state["mi_clinica"] = Veterinaria()
 
@@ -31,8 +35,8 @@ else:
     password = st.text_input("Contraseña", type="password")
 
     if st.button("Iniciar Sesión"):
-        # Las credenciales son : ADMIN / Curae1234
-        if usuario == "ADMIN" and password == "Curae1234":
+        # Las credenciales son : ADMIN y 1234
+        if usuario == "ADMIN" and password == "1234":
             st.session_state["login_correcto"] = True
             st.success("¡Login correcto! Accediendo...")
             st.balloons()
